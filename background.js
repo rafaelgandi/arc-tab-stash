319e776000d06014776b11e2c0c47f8f8fb0b06b
rafaelgandi.mac <rafaelgandi@gmail.com>
2023-01-27 18:42:29 +0800

    started

--- background.js
+++ background.js
@@ -1,12 +1,43 @@
 
 
-console.log(111122);
-
 (async () => {
+    // console.log(chrome.runtime);
+    // setTimeout(() => {
+    //     chrome.runtime.sendMessage(chrome.runtime.id, {},  () => {});
+    // }, 1000);
+    
 
-    console.log(chrome.runtime.id);
-    console.log(await chrome.runtime.getPlatformInfo());
+    // See: https://dev.to/paulasantamaria/adding-shortcuts-to-your-chrome-extension-2i20
+    chrome.commands.onCommand.addListener(async (command) => {
+        switch (command) {
+            case 'stash-current-tab':
+                await handleStashingTab();
+                break;
+            default:
+                console.log(`Command ${command} not found`);
+        }
+    });
 
-    const tabs = await chrome.tabs.query({});
-    console.dir(tabs.map((t) => t.title));
+    chrome.runtime.onMessage.addListener(async (data, sender, sendResponse) => {
+        if (data.message === 'get-user-tab-stash-from-notion') {
+            sendResponse('heyyyyy');
+        }
+    });
+
+    async function getCurrentTabData() {
+        // See: https://stackoverflow.com/a/17826527
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        return tabs?.[0] ?? undefined;
+    }
+
+    async function handleStashingTab() {
+        const tab = await getCurrentTabData();
+        if (!tab) { return; }
+        const { favIconUrl, title, url, id } = tab;
+
+        chrome.tabs.sendMessage(id, {
+             message: tab
+        }, (response) => {});
+    }
+
 })();
--- content.js
+++ content.js
@@ -1,4 +1,12 @@
 /* This runs after a web page loads */
-console.log('heheheheheh');
 
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message, 'hohoho');
+});
+
+
+chrome.runtime.sendMessage({message: "get-user-tab-stash-from-notion"}, (response) => {
+    console.log(response);
+});
 
