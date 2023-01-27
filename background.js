c13e3c92f8e76aef81ae66ef59efe2fd5bfc65e3
rafaelgandi.mac <rafaelgandi@gmail.com>
2023-01-27 23:55:45 +0800

    connecting to notion api

--- background.js
+++ background.js
@@ -5,7 +5,7 @@
     // setTimeout(() => {
     //     chrome.runtime.sendMessage(chrome.runtime.id, {},  () => {});
     // }, 1000);
-    
+
 
     // See: https://dev.to/paulasantamaria/adding-shortcuts-to-your-chrome-extension-2i20
     chrome.commands.onCommand.addListener(async (command) => {
@@ -20,6 +20,21 @@
 
     chrome.runtime.onMessage.addListener(async (data, sender, sendResponse) => {
         if (data.message === 'get-user-tab-stash-from-notion') {
+            const token = 'secret_7gjlSgBMJPbQdmfSUFP5ZsXFB7sl10RRgcefv7qHxq8';
+            const docId = 'c5fcca844538418eb8666b63d9050543';
+            const res = await fetch(`https://api.notion.com/v1/blocks/${docId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                        "Notion-Version": "2022-02-22",
+                    }
+                });
+            const block = await res.json();    
+            console.log(block);
+            console.log(JSON.parse(block.code['rich_text'][0]['plain_text']));    
             sendResponse('heyyyyy');
         }
     });
@@ -36,8 +51,8 @@
         const { favIconUrl, title, url, id } = tab;
 
         chrome.tabs.sendMessage(id, {
-             message: tab
-        }, (response) => {});
+            message: tab
+        }, (response) => { });
     }
 
 })();
