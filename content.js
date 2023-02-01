dc2626421d484f8c6857541a9043cdcc8117de0d
rafaelgandi.mac <rafaelgandi@gmail.com>
2023-02-01 18:19:49 +0800

    added toast

--- content.js
+++ content.js
@@ -1,29 +1,50 @@
 /* This runs after a web page loads */
 (async () => {
-
     // Importing from helper scripts
     // See: https://stackoverflow.com/a/53033388
     const helpers = await import(chrome.runtime.getURL('lib/helpers.js'));
 
+    function getArcPalette() {
+        return {
+            'arcPaletteTitle': getArcColor('--arc-palette-title'),
+            'arcBGGradients': [
+                getArcColor('--arc-background-gradient-color0'),
+                getArcColor('--arc-background-gradient-color1'),
+                getArcColor('--arc-background-gradient-color2'),
+                getArcColor('--arc-background-gradient-overlay-color0'),
+                getArcColor('--arc-background-gradient-overlay-color1'),
+                getArcColor('--arc-background-gradient-overlay-color2')
+            ]
+        };
+    }
+
+    function presentToast(msg) {
+        const { arcPaletteTitle, arcBGGradients } = getArcPalette();
+        Toastify({
+            text: msg,
+            duration: 5000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            className: '__stash-toast',
+            style: {   
+                color: arcPaletteTitle ?? '#092609',            
+                background: `linear-gradient(140deg, ${arcBGGradients.filter((color) => !!color).join(', ')})`,
+            },
+            onClick: function () { } // Callback after click
+        }).showToast();
+    }
 
     chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
         // console.log(data, 'hohoho');
         if (data.message === 'get-arc-colors') {
             // See: https://arc.net/colors.html
-            sendResponse({
-                'arcPaletteTitle': getArcColor('--arc-palette-title'),
-                'arcBGGradients': [
-                    getArcColor('--arc-background-gradient-color0'),
-                    getArcColor('--arc-background-gradient-color1'),
-                    getArcColor('--arc-background-gradient-color2'),
-                    getArcColor('--arc-background-gradient-overlay-color0'),
-                    getArcColor('--arc-background-gradient-overlay-color1'),
-                    getArcColor('--arc-background-gradient-overlay-color2')
-                ]
-            });
+            sendResponse(getArcPalette());
         }
         if (data.message === 'tab-added-to-stash') {
             // alert('Tab added to stash.');
+            presentToast('Added to stash 👍');
         }
         if (data.message === 'log-please') {
             console.log(data.data);
@@ -37,6 +58,6 @@
         return document.documentElement.style.getPropertyValue(name) ?? null;
     }
 
-    const response = await helpers.sendMessageToBg({message: "get-user-tab-stash-from-notion"});
+    const response = await helpers.sendMessageToBg({ message: "get-user-tab-stash-from-notion" });
 
 })();
